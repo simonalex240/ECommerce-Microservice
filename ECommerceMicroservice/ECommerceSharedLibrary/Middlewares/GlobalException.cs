888692e86cf49a9f646ef31dfa63b5a91f353105
simonalex240 <simonnjorogethe4th@gmail.com>
2025-04-22 19:59:52 +0300

    Update GlobalException.cs
    Updating the Global Exception

--- ECommerceMicroservice/ECommerceSharedLibrary/Middlewares/GlobalException.cs
+++ ECommerceMicroservice/ECommerceSharedLibrary/Middlewares/GlobalException.cs
@@ -8,32 +8,20 @@ namespace ECommerceSharedLibrary.Middlewares
 {
     public class GlobalException(RequestDelegate next)
     {
-        /*
-        The middle ware named GlobalException is a middle ware that helps us catch Exceptions that may have slipped up from local middlewares i.e. try and catch blocks.
-        We take in RequestDelegate which I understand is a function that helps us call the next function in the middleware chain. (Expound on this )
-        */
+      
         public async Task InvokeAsync(HttpContext context)
         {
-            /*
-            The invoke function is a function that helps us catch those errors if any exists and helps us forward a scary free message that the users can understand.
-            What is the HttpContext class and how does it function ?
-            */
+            
             string title = "Error";
             string message = "Sorry, Internal server Error. Kindly try again";
             int statusCode = (int)HttpStatusCode.InternalServerError;
 
-            /*
-            The default errors will be the ones that we have predefined above i.e. (title message and statusCode).
-            Why do we cast the HttpStatusCode.InternalServerError in to an integer and what type of error would we get if this is not done ?
-            */
+            
 
             try
             {
                 await next(context);
-                /*
-                    We will first await the response from the chain.
-                    We then run this response form context through a couple of if statements and check through the Response in the context when the Status codes matches one of our status codes. If it does we reassign the variables to match the out come and call the ModifyHeader with the new items for it to display a scary free message to the User.
-                */
+               
 
                 if (context.Response.StatusCode == StatusCodes.Status429TooManyRequests)
                 {
@@ -62,14 +50,7 @@ namespace ECommerceSharedLibrary.Middlewares
             }
             catch (Exception ex)
             {
-                /*
-                If the Exceptions status Code do not match any of the above statusCodes it comes to the catch portion of the code where we first,
-                using Logger which is a log exception handler we created, log this error to the developer.
-                We then check if the exception matches the TaskCanceledException or TimeoutException exception which means some thing might be wrong with the server.
-                (
-                    Explain why we do not use the status code and what are TaskCanceledException and TimeoutException exceptions.
-                )
-                */
+               
                 Loggers(ex);
 
                 if (ex is TaskCanceledException || ex is TimeoutException)
@@ -78,9 +59,7 @@ namespace ECommerceSharedLibrary.Middlewares
                     message = "Request timeout .. tey again";
                     statusCode = StatusCodes.Status408RequestTimeout;
                 }
-                /*
-                We then call the ModifyHeader method as the default operation.
-                */
+                
                 await ModifyHeader(context, title, message, statusCode);
             }
         }
@@ -93,10 +72,7 @@ namespace ECommerceSharedLibrary.Middlewares
         )
         {
             context.Response.ContentType = "application/json";
-            /*
-            The ModifyHeader is the message that creates the scary free message.
-            What I do not understand here is the JsonSerializer and ProblemDetails ad finally the CancellationToken. Explain them in detail and what they are in the code for.
-             */
+            
             await context.Response.WriteAsync(
                 JsonSerializer.Serialize(
                     new ProblemDetails()
